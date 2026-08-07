e158df22db43f3f1b8622cefd1d8247276a9da61
juansopo <sopojuanesteban@gmail.com>
2026-08-07 17:39:13 -0300

    se configura el appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { HealthModule } from './health/health.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,7 +11,23 @@ import { HealthModule } from './health/health.module';
       isGlobal: true,
       load: [configuration],
     }),
+
+  TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'mysql',
+    host: config.get<string>('database.host'),
+    port: config.get<number>('database.port'),
+    username: config.get<string>('database.username'),
+    password: config.get<string>('database.password'),
+    database: config.get<string>('database.name'),
+    autoLoadEntities: true,
+    synchronize: false,
+  }),
+}),
+
     HealthModule,
+    UsuariosModule,
   ],
 })
 export class AppModule {}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,14 @@ export interface AppConfig {
   env: string;
 }
 
-export default (): AppConfig => ({
+export default () => ({
   port: Number(process.env.PORT) || 3000,
   env: process.env.NODE_ENV ?? 'development',
+  database: {
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER ?? 'root',
+    password: process.env.DB_PASSWORD ?? '',
+    name: process.env.DB_NAME ?? 'lution_dsw',
+  },
 });
